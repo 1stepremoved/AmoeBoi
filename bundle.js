555c4cd834af20dd45a0f3aeb4f25a2c200cc268
Hamilton Sands <hamiltonbsands@gmail.com>
2018-01-21 20:32:49 -0500

    more file reorganization

--- bundle.js
+++ bundle.js
@@ -60,7 +60,7 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -68,178 +68,35 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__amoeba_js__ = __webpack_require__(1);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__amoeboi_js__ = __webpack_require__(3);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__util__ = __webpack_require__(2);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__game__ = __webpack_require__(4);
-
-
-
-
-
-
-window.maxZoom = 4;
-window.minZoom = 0.7;
-window.currentZoom = window.maxZoom;
-window.realBoardHeight = 20000;
-window.realBoardWidth = 20000;
-window.boardHeight = window.realBoardHeight / window.currentZoom;
-window.boardWidth = window.realBoardWidth / window.currentZoom;
-window.boardFocus = {x: window.realBoardWidth / 2, y: window.realBoardHeight / 2};
-window.timeBase = 10;
-window.timeCoefficient = .2;
-window.clockAngle = 0;
-window.baseMass = 50000;
-window.mouseDownTime = null;
-window.mouseDownInterval = null;
-window.paused = false;
-window.status = "playing";
-
-document.addEventListener("DOMContentLoaded", () => {
-  window.onresize = ()=>{
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-  };
-
-  window.addEventListener("mousedown", (e) => {
-    if (window.paused || window.status !== "playing") {
-      return;
-    }
-    window.mouseDownTime = Date.now();
-    window.amoeboi.mousePosX = e.pageX;
-    window.amoeboi.mousePosY = e.pageY;
-    window.amoeboi.propel(e,window.amoebas);
-    window.mouseDownInterval = setInterval(() => {
-      window.amoeboi.propel(e,window.amoebas);
-    }, 200);
-  });
-
-  window.addEventListener("mousemove", (e) => {
-    window.mousePos = {x: e.pageX, y: e.pageY};
-    if (window.mouseDownTime) {
-      window.amoeboi.mousePosX = e.pageX;
-      window.amoeboi.mousePosY = e.pageY;
-    }
-  });
-
-  window.addEventListener("mouseup", (e) => {
-    window.mouseDownTime = null;
-    clearInterval(window.mouseDownInterval);
-  });
-
-  window.addEventListener("keydown", (e) => {
-    switch (e.keyCode) {
-      case 39:
-        if (window.paused || window.status !== "playing") { return; }
-        window.timeCoefficient = Math.min(window.timeCoefficient * 1.1, window.timeBase);
-        return;
-      case 37:
-        if (window.paused || window.status !== "playing") { return; }
-        window.timeCoefficient = Math.max(window.timeCoefficient * 0.9, Math.pow(window.timeBase, - 1));
-        return;
-      case 32:
-        if (window.status !== "playing") { return; }
-        window.paused = !window.paused;
-        window.mouseDownTime = null;
-        return;
-      case 72:
-        window.status = "reset";
-        window.paused = false;
-        return;
-      default:
-        return;
-    }
-  });
-
-  document.addEventListener("mousewheel", (e)=> {
-    e.preventDefault();
-    if (window.paused  || window.status !== "playing") { return; }
-    let zoomDelta = (e.deltaY / -1000);
-    window.currentZoom = Object(__WEBPACK_IMPORTED_MODULE_2__util__["b" /* boundNum */])(window.currentZoom + zoomDelta, window.minZoom, window.maxZoom);
-    window.boardHeight = window.realBoardHeight / window.currentZoom;
-    window.boardWidth = window.realBoardWidth / window.currentZoom;
-  });
-
-  const canvas = document.getElementById("background");
-  const ctx = canvas.getContext("2d");
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-
-  window.amoebas = [];
-  window.amoeboi = new __WEBPACK_IMPORTED_MODULE_1__amoeboi_js__["a" /* default */](ctx, window.realBoardWidth / 2, window.realBoardHeight / 2, 100000, {x: 100000, y: 0});
-  for (let i = 0; i < 400; i++) {
-    window.amoebas.push(new __WEBPACK_IMPORTED_MODULE_0__amoeba_js__["a" /* default */](ctx));
+const boundNum = (num, min, max) => {
+  if (num > max) {
+    return max;
+  } else if (num < min) {
+    return min;
+  } else {
+    return num;
   }
-  // amoebas.push(new Amoeba(ctx, 4500, 5000, 100000, {x: 100000, y: 0}));
-  // amoebas.push(new Amoeba(ctx, 5500, 5000, 300000, {x: -100000, y: 0}));
-  // amoebas.push(new Amoeba(ctx, 5300, 5000, 100000, {x: -100000, y: 0}));
-  let animate = () => {
-    ctx.clearRect(0,0, innerWidth, innerHeight);
-    Object(__WEBPACK_IMPORTED_MODULE_3__game__["a" /* makeGrid */])(ctx);
+};
+/* harmony export (immutable) */ __webpack_exports__["b"] = boundNum;
 
-    if (window.status === "reset") {
-      window.maxZoom = 4;
-      window.currentZoom = 2;
-      window.boardHeight = window.realBoardHeight / window.currentZoom;
-      window.boardWidth = window.realBoardWidth / window.currentZoom;
-      window.amoeboi = null;
-      window.amoebas = [];
-      for (let i = 0; i < 400; i++) {
-        window.amoebas.push(new __WEBPACK_IMPORTED_MODULE_0__amoeba_js__["a" /* default */](ctx));
-      }
-      window.boardFocus = {x: window.realBoardWidth / 2, y: window.realBoardHeight / 2};
-      window.status = "homescreen";
-      return requestAnimationFrame(animate);
-    }
 
-    if (window.status === "homescreen") {
-      Object(__WEBPACK_IMPORTED_MODULE_3__game__["e" /* moveAmoebas */])(ctx);
-      Object(__WEBPACK_IMPORTED_MODULE_3__game__["b" /* makeHomescreen */])(ctx);
-      return requestAnimationFrame(animate);
-    }
+const baseLog = (x, y) => {
+  return Math.log(y) / Math.log(x);
+};
+/* harmony export (immutable) */ __webpack_exports__["a"] = baseLog;
 
-    if (window.paused) {
-      window.amoebas.forEach(amoeba => {
-        amoeba.draw();
-      });
-      ctx.globalAlpha = 0.7;
-      ctx.fillStyle = "black";
-      ctx.fillRect(0,0, innerWidth, innerHeight);
-      window.amoeboi.draw();
-      Object(__WEBPACK_IMPORTED_MODULE_3__game__["c" /* makeMargins */])(ctx);
-      return requestAnimationFrame(animate);
-    }
 
-    Object(__WEBPACK_IMPORTED_MODULE_3__game__["e" /* moveAmoebas */])(ctx);
+const transitionVar = (variable, start, stop, rate = .01) => {
+  const isGoingUp = (start < stop);
+  const transitionRate = (isGoingUp) ? 1 + rate : 1 - rate;
+  if ((isGoingUp && start > stop * .99) || (!isGoingUp && start < stop * 1.01)) {
+    return variable;
+  } else {
+    return variable * transitionRate;
+  }
+};
+/* unused harmony export transitionVar */
 
-    Object(__WEBPACK_IMPORTED_MODULE_3__game__["c" /* makeMargins */])(ctx);
-    Object(__WEBPACK_IMPORTED_MODULE_3__game__["d" /* makeMassDisplay */])(ctx);
-    // makeClock(ctx);
-    if (window.amoeboi.mass > 0) {
-      window.boardFocus = {x: window.amoeboi.xpos, y: window.amoeboi.ypos};
-      window.baseMass = window.amoeboi.mass;
-      if (window.amoeboi.radius / window.realBoardWidth * 1000 * window.currentZoom > 75) {
-        window.maxZoom = Object(__WEBPACK_IMPORTED_MODULE_2__util__["b" /* boundNum */])(75 / (window.amoeboi.radius / window.realBoardWidth * 1000), 1, 4);
-        window.currentZoom = Object(__WEBPACK_IMPORTED_MODULE_2__util__["b" /* boundNum */])(window.currentZoom * 0.999, window.minZoom, window.maxZoom);
-        window.boardHeight = window.realBoardHeight / window.currentZoom;
-        window.boardWidth = window.realBoardWidth / window.currentZoom;
-      }
-      if (window.amoeboi.radius / window.realBoardWidth * 1000 * window.maxZoom < 75) {
-        window.maxZoom = Object(__WEBPACK_IMPORTED_MODULE_2__util__["b" /* boundNum */])(75 / (window.amoeboi.radius / window.realBoardWidth * 1000), 1, 4);
-      }
-    } else {
-      window.boardFocus['x'] += (window.boardFocus['x'] < window.realBoardWidth / 2) ? 10 : -10;
-      window.boardFocus['y'] += (window.boardFocus['y'] < window.realBoardHeight / 2) ? 10 : -10;
-      window.currentZoom = window.currentZoom > 1 ? window.currentZoom * 0.9 : window.currentZoom;
-      window.boardHeight = window.realBoardHeight / window.currentZoom;
-      window.boardWidth = window.realBoardWidth / window.currentZoom;
-      window.baseMass = 0;
-    }
-    requestAnimationFrame(animate);
-  };
-  requestAnimationFrame(animate);
-});
 
 
 /***/ }),
@@ -247,7 +104,7 @@ document.addEventListener("DOMContentLoaded", () => {
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__util__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__util__ = __webpack_require__(0);
 
 
 class Amoeba {
@@ -401,35 +258,179 @@ Amoeba.prototype.momentumMax = 10;
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-const boundNum = (num, min, max) => {
-  if (num > max) {
-    return max;
-  } else if (num < min) {
-    return min;
-  } else {
-    return num;
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__amoeba_js__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__amoeboi_js__ = __webpack_require__(3);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__util__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__game__ = __webpack_require__(4);
+
+
+
+
+
+
+window.maxZoom = 4;
+window.minZoom = 0.7;
+window.currentZoom = window.maxZoom;
+window.realBoardHeight = 20000;
+window.realBoardWidth = 20000;
+window.boardHeight = window.realBoardHeight / window.currentZoom;
+window.boardWidth = window.realBoardWidth / window.currentZoom;
+window.boardFocus = {x: window.realBoardWidth / 2, y: window.realBoardHeight / 2};
+window.timeBase = 10;
+window.timeCoefficient = .2;
+window.clockAngle = 0;
+window.baseMass = 50000;
+window.mouseDownTime = null;
+window.mouseDownInterval = null;
+window.paused = false;
+window.status = "playing";
+window.mousePos= {x: 0, y: 0}
+document.addEventListener("DOMContentLoaded", () => {
+  window.onresize = ()=>{
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  };
+
+  window.addEventListener("mousedown", (e) => {
+    if (window.paused || window.status !== "playing") {
+      return;
+    }
+    window.mouseDownTime = Date.now();
+    window.amoeboi.mousePosX = e.pageX;
+    window.amoeboi.mousePosY = e.pageY;
+    window.amoeboi.propel(e,window.amoebas);
+    window.mouseDownInterval = setInterval(() => {
+      window.amoeboi.propel(e,window.amoebas);
+    }, 200);
+  });
+
+  window.addEventListener("mousemove", (e) => {
+    window.mousePos = {x: e.pageX, y: e.pageY};
+    if (window.mouseDownTime) {
+      window.amoeboi.mousePosX = e.pageX;
+      window.amoeboi.mousePosY = e.pageY;
+    }
+  });
+
+  window.addEventListener("mouseup", (e) => {
+    window.mouseDownTime = null;
+    clearInterval(window.mouseDownInterval);
+  });
+
+  window.addEventListener("keydown", (e) => {
+    switch (e.keyCode) {
+      case 39:
+        if (window.paused || window.status !== "playing") { return; }
+        window.timeCoefficient = Math.min(window.timeCoefficient * 1.1, window.timeBase);
+        return;
+      case 37:
+        if (window.paused || window.status !== "playing") { return; }
+        window.timeCoefficient = Math.max(window.timeCoefficient * 0.9, Math.pow(window.timeBase, - 1));
+        return;
+      case 32:
+        if (window.status !== "playing") { return; }
+        window.paused = !window.paused;
+        window.mouseDownTime = null;
+        return;
+      case 72:
+        window.status = "reset";
+        window.paused = false;
+        return;
+      default:
+        return;
+    }
+  });
+
+  document.addEventListener("mousewheel", (e)=> {
+    e.preventDefault();
+    if (window.paused  || window.status !== "playing") { return; }
+    let zoomDelta = (e.deltaY / -1000);
+    window.currentZoom = Object(__WEBPACK_IMPORTED_MODULE_2__util__["b" /* boundNum */])(window.currentZoom + zoomDelta, window.minZoom, window.maxZoom);
+    window.boardHeight = window.realBoardHeight / window.currentZoom;
+    window.boardWidth = window.realBoardWidth / window.currentZoom;
+  });
+
+  const canvas = document.getElementById("background");
+  const ctx = canvas.getContext("2d");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  window.amoebas = [];
+  window.amoeboi = new __WEBPACK_IMPORTED_MODULE_1__amoeboi_js__["a" /* default */](ctx, window.realBoardWidth / 2, window.realBoardHeight / 2, 100000, {x: 100000, y: 0});
+  for (let i = 0; i < 400; i++) {
+    window.amoebas.push(new __WEBPACK_IMPORTED_MODULE_0__amoeba_js__["a" /* default */](ctx));
   }
-};
-/* harmony export (immutable) */ __webpack_exports__["b"] = boundNum;
+  // amoebas.push(new Amoeba(ctx, 4500, 5000, 100000, {x: 100000, y: 0}));
+  // amoebas.push(new Amoeba(ctx, 5500, 5000, 300000, {x: -100000, y: 0}));
+  // amoebas.push(new Amoeba(ctx, 5300, 5000, 100000, {x: -100000, y: 0}));
+  let animate = () => {
+    ctx.clearRect(0,0, innerWidth, innerHeight);
+    Object(__WEBPACK_IMPORTED_MODULE_3__game__["a" /* makeGrid */])(ctx);
 
+    if (window.status === "reset") {
+      window.maxZoom = 4;
+      window.currentZoom = 2;
+      window.boardHeight = window.realBoardHeight / window.currentZoom;
+      window.boardWidth = window.realBoardWidth / window.currentZoom;
+      window.baseMass = 50000;
+      window.amoeboi = null;
+      window.amoebas = [];
+      for (let i = 0; i < 400; i++) {
+        window.amoebas.push(new __WEBPACK_IMPORTED_MODULE_0__amoeba_js__["a" /* default */](ctx));
+      }
+      window.boardFocus = {x: window.realBoardWidth / 2, y: window.realBoardHeight / 2};
+      window.status = "homescreen";
+      return requestAnimationFrame(animate);
+    }
 
-const baseLog = (x, y) => {
-  return Math.log(y) / Math.log(x);
-};
-/* harmony export (immutable) */ __webpack_exports__["a"] = baseLog;
+    if (window.status === "homescreen") {
+      Object(__WEBPACK_IMPORTED_MODULE_3__game__["e" /* moveAmoebas */])(ctx);
+      Object(__WEBPACK_IMPORTED_MODULE_3__game__["b" /* makeHomescreen */])(ctx);
+      return requestAnimationFrame(animate);
+    }
 
+    if (window.paused) {
+      window.amoebas.forEach(amoeba => {
+        amoeba.draw();
+      });
+      ctx.globalAlpha = 0.7;
+      ctx.fillStyle = "black";
+      ctx.fillRect(0,0, innerWidth, innerHeight);
+      window.amoeboi.draw();
+      Object(__WEBPACK_IMPORTED_MODULE_3__game__["c" /* makeMargins */])(ctx);
+      return requestAnimationFrame(animate);
+    }
 
-const transitionVar = (variable, start, stop, rate = .01) => {
-  const isGoingUp = (start < stop);
-  const transitionRate = (isGoingUp) ? 1 + rate : 1 - rate;
-  if ((isGoingUp && start > stop * .99) || (!isGoingUp && start < stop * 1.01)) {
-    return variable;
-  } else {
-    return variable * transitionRate;
-  }
-};
-/* unused harmony export transitionVar */
+    Object(__WEBPACK_IMPORTED_MODULE_3__game__["e" /* moveAmoebas */])(ctx);
 
+    Object(__WEBPACK_IMPORTED_MODULE_3__game__["c" /* makeMargins */])(ctx);
+    Object(__WEBPACK_IMPORTED_MODULE_3__game__["d" /* makeMassDisplay */])(ctx);
+    // makeClock(ctx);
+    if (window.amoeboi.mass > 0) {
+      window.boardFocus = {x: window.amoeboi.xpos, y: window.amoeboi.ypos};
+      window.baseMass = window.amoeboi.mass;
+      if (window.amoeboi.radius / window.realBoardWidth * 1000 * window.currentZoom > 75) {
+        window.maxZoom = Object(__WEBPACK_IMPORTED_MODULE_2__util__["b" /* boundNum */])(75 / (window.amoeboi.radius / window.realBoardWidth * 1000), 1, 4);
+        window.currentZoom = Object(__WEBPACK_IMPORTED_MODULE_2__util__["b" /* boundNum */])(window.currentZoom * 0.999, window.minZoom, window.maxZoom);
+        window.boardHeight = window.realBoardHeight / window.currentZoom;
+        window.boardWidth = window.realBoardWidth / window.currentZoom;
+      }
+      if (window.amoeboi.radius / window.realBoardWidth * 1000 * window.maxZoom < 75) {
+        window.maxZoom = Object(__WEBPACK_IMPORTED_MODULE_2__util__["b" /* boundNum */])(75 / (window.amoeboi.radius / window.realBoardWidth * 1000), 1, 4);
+      }
+    } else {
+      window.boardFocus['x'] += (window.boardFocus['x'] < window.realBoardWidth / 2) ? 10 : -10;
+      window.boardFocus['y'] += (window.boardFocus['y'] < window.realBoardHeight / 2) ? 10 : -10;
+      window.currentZoom = window.currentZoom > 1 ? window.currentZoom * 0.9 : window.currentZoom;
+      window.boardHeight = window.realBoardHeight / window.currentZoom;
+      window.boardWidth = window.realBoardWidth / window.currentZoom;
+      window.baseMass = 0;
+    }
+    requestAnimationFrame(animate);
+  };
+  requestAnimationFrame(animate);
+});
 
 
 /***/ }),
@@ -438,7 +439,7 @@ const transitionVar = (variable, start, stop, rate = .01) => {
 
 "use strict";
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__amoeba__ = __webpack_require__(1);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__util__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__util__ = __webpack_require__(0);
 
 
 
@@ -493,7 +494,7 @@ class Amoeboi extends __WEBPACK_IMPORTED_MODULE_0__amoeba__["a" /* default */] {
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__util__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__util__ = __webpack_require__(0);
 
 
 const makePause = (ctx) => {
