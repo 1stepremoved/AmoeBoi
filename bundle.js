99f5019e3f55bb805808500aaac72f28143744a2
Hamilton Sands <hamiltonbsands@gmail.com>
2018-01-26 14:20:54 -0500

    insertion working it seems, collision methods untested

--- bundle.js
+++ bundle.js
@@ -514,6 +514,8 @@ document.addEventListener("DOMContentLoaded", () => {
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__util__ = __webpack_require__(0);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__amoeba_js__ = __webpack_require__(1);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__amoeboi_js__ = __webpack_require__(2);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__quadtree_js__ = __webpack_require__(5);
+
 
 
 
@@ -595,6 +597,7 @@ class Game {
     this.amoeboi = !amoeboi ? null : new __WEBPACK_IMPORTED_MODULE_2__amoeboi_js__["a" /* default */](this.ctx, this.boardVars.realBoardWidth / 2,
                                                  this.boardVars.realBoardHeight / 2,
                                                  100000, {x: 0, y: 0});
+    this.quadTree = new __WEBPACK_IMPORTED_MODULE_3__quadtree_js__["a" /* default */](0,0, this.boardVars.realBoardWidth, this.boardVars.realBoardHeight);
     let mass, radius, xpos, ypos, momentum;
     for (let i = 0; i < 2; i++) {
       mass = Math.floor((Math.random() * 40000000) + 10000000);
@@ -636,6 +639,19 @@ class Game {
       momentum = {x: Math.floor(Math.random() * 500000) - 250000, y: Math.floor(Math.random() * 500000) - 250000};
       this.amoebas.push(new __WEBPACK_IMPORTED_MODULE_1__amoeba_js__["a" /* default */](this.ctx, xpos, ypos, mass));
     }
+
+    // for (let i = 0; i < 20; i++) {
+    //   mass = Math.floor((Math.random() * 10000) + 10000);
+    //   radius = Math.sqrt(mass / (Math.PI));
+    //   xpos = Math.floor(Math.random() * (this.boardVars.realBoardWidth - radius)) + radius;
+    //   ypos = Math.floor(Math.random() * (this.boardVars.realBoardHeight - radius)) + radius;
+    //   momentum = {x: Math.floor(Math.random() * 500000) - 250000, y: Math.floor(Math.random() * 500000) - 250000};
+    //   this.amoebas.push(new Amoeba(this.ctx, xpos, ypos, mass));
+    // }
+    for (let i = 0; i < this.amoebas.length; i++) {
+      this.quadTree.insert1(this.amoebas[i]);
+    }
+    debugger
   }
 
   animate() {
@@ -957,6 +973,131 @@ class Game {
 
 
 /* harmony default export */ __webpack_exports__["a"] = (Game);
+
+
+/***/ }),
+/* 5 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__amoeba__ = __webpack_require__(1);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__amoeboi__ = __webpack_require__(2);
+
+
+
+class QuadTree {
+  constructor(x, y, width,height,level = 0) {
+    this.children = [];
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.endX = this.x + this.width;
+    this.endY = this.y + this.height;
+    this.halfX = this.x +  Math.floor(this.width / 2);
+    this.halfY = this.y +  Math.floor(this.height / 2);
+    this.level = level;
+
+    this.amoebas = [];
+  }
+
+  split() {
+    let halfWidth = Math.floor(this.width / 2);
+    let halfHeight = Math.floor(this.height / 2);
+    this.children.push(new QuadTree(this.x, this.y, halfWidth , halfHeight, this.level + 1));
+    this.children.push(new QuadTree(this.x + halfWidth, this.y, halfWidth , halfHeight, this.level + 1));
+    this.children.push(new QuadTree(this.x, this.y + halfHeight, halfWidth , halfHeight, this.level + 1));
+    this.children.push(new QuadTree(this.x + halfWidth, this.y + halfHeight, halfWidth , halfHeight, this.level + 1));
+  }
+
+  clear() {
+    this.amoebas = [];
+    for (let i = 0; i < this.children.length; i++) {
+      this.children[i].clear();
+      this.children[i] = null;
+    }
+    if (this.level === 0) {
+      this.children = [];
+    }
+  }
+
+  insert1(amoeba) {
+    let index = this.getIndex(amoeba);
+    if (this.children.length > 0 && index !== -1) {
+      return this.children[index].insert1(amoeba);
+    }
+
+    this.amoebas.push(amoeba);
+
+    if (this.children.length === 0 && this.amoebas.length > this.maxAmoebas) {
+      this.split();
+      let newAmoebas = [];
+      for (let i = 0; i < this.amoebas.length; i++) {
+        // debugger
+        index = this.getIndex(this.amoebas[i]);
+        if (index !== -1) {
+          this.children[index].insert1(this.amoebas[i]);
+        } else {
+          newAmoebas.push(this.amoebas[i]);
+        }
+      }
+      this.amoebas = newAmoebas;
+    }
+
+  }
+
+  getIndex(amoeba) {
+    if ((amoeba.xpos - amoeba.radius) > this.x && (amoeba.xpos + amoeba.radius) < this.halfX
+     && (amoeba.ypos - amoeba.radius) > this.y && (amoeba.ypos + amoeba.radius) < this.halfY) {
+       return 0;
+    } else if ((amoeba.xpos - amoeba.radius) > this.halfX && (amoeba.xpos + amoeba.radius) < this.endX
+     && (amoeba.ypos - amoeba.radius) > this.y && (amoeba.ypos + amoeba.radius) < this.halfY) {
+       return 1;
+    } else if ((amoeba.xpos - amoeba.radius) > this.x && (amoeba.xpos + amoeba.radius) < this.halfX
+     && (amoeba.ypos - amoeba.radius) > this.halfY && (amoeba.ypos + amoeba.radius) < this.endY) {
+       return 2;
+    } else if ((amoeba.xpos - amoeba.radius) > this.halfX && (amoeba.xpos + amoeba.radius) < this.endX
+     && (amoeba.ypos - amoeba.radius) > this.halfY && (amoeba.ypos + amoeba.radius) < this.endY) {
+       return 3;
+    } else {
+      return -1;
+    }
+  }
+
+  checkAllCollisions() {
+    for (let i = 0; i < this.amoebas.length; i++) {
+      for (let j = 0; j < this.amoebas.length; j++) {
+        if (i === j) {
+          continue;
+        }
+        this.amoebas[i].aabbCheck(this.amoebas[j]);
+      }
+      for (let j = 0; j < this.children.length; j++) {
+        this.children[i].checkCollision(this.amoebas[i]);
+      }
+    }
+
+    for (let i = 0; i < this.children.length; i++) {
+      this.children[i].checkAllCollisions();
+    }
+  }
+
+  checkCollision(amoeba) {
+    for (let i = 0; i < this.amoebas.length; i++) {
+      amoeba.aabbCheck(this.amoebas[i]);
+    }
+
+    for (let i = 0; i < this.children.length; i++) {
+      this.children[i].checkCollision(amoeba);
+    }
+
+  }
+
+}
+
+QuadTree.prototype.maxAmoebas = 10;
+
+/* harmony default export */ __webpack_exports__["a"] = (QuadTree);
 
 
 /***/ })
