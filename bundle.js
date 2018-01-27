f9c873e607c95b5c094b5c5f45c6294d5cca6d03
Hamilton Sands <hamiltonbsands@gmail.com>
2018-01-27 15:34:40 -0500

    for smoother absorbtion, reduced maximum bubble size by order of magnitude. does not seem to affect gameplay

--- bundle.js
+++ bundle.js
@@ -186,7 +186,7 @@ class Amoeba {
         }
 
         let bubble = this.massDelta * this.mass
-            * Object(__WEBPACK_IMPORTED_MODULE_0__util__["b" /* boundNum */])( (this.radius - (currentDistance - amoeba.radius)) / this.radius, .1, 1);
+            * Object(__WEBPACK_IMPORTED_MODULE_0__util__["b" /* boundNum */])( (this.radius - (currentDistance - amoeba.radius)) / this.radius, .01, .1);
 
         this.mass -= bubble;
         amoeba.mass += bubble;
@@ -201,7 +201,7 @@ class Amoeba {
         }
 
         let bubble = amoeba.massDelta * amoeba.mass
-            * Object(__WEBPACK_IMPORTED_MODULE_0__util__["b" /* boundNum */])( (amoeba.radius - (currentDistance - this.radius)) / amoeba.radius, .1, 1);
+            * Object(__WEBPACK_IMPORTED_MODULE_0__util__["b" /* boundNum */])( (amoeba.radius - (currentDistance - this.radius)) / amoeba.radius, .01, .1);
 
         amoeba.mass -= bubble;
         this.mass += bubble;
