57c1786aa893f1db02c01799143c0f0981069fb0
Hamilton Sands <hamilton.sands@publicismedia.com>
2020-01-18 16:13:30 -0500

    add babel and class arrow functions

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,23 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
   devtool: 'source-map',
   resolve: {
     extensions: ['.js', '*']
